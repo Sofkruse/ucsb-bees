a54d076561b040ece19496fdab6c706b9e5b9920
Sofkruse <122656332+Sofkruse@users.noreply.github.com>
2023-02-03 17:13:16 -0800

    more files

---
+++ native plants chart without locations copy.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  3 16:46:38 2023
+
+@author: sofiakruse
+"""
+
+import pandas as pd
+data = pd.read_csv("chart copy.csv")
+print(data)
