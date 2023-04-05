d32fc17d13070fdd5342e4771f22873fbd2a8dab
RyanZanone <122947907+RyanZanone@users.noreply.github.com>
2023-04-04 21:56:37 -0700

    Add files via upload

--- scraping.py
+++ scraping.py
@@ -3,31 +3,33 @@ from selenium.webdriver.common.by import By
 import pandas as pd
 
 
-url = 'https://calscape.org/loc-34.4136,-119.8497%20(93106)/cat-All-Plants/ord-popular?srchcr=sc63f5693746598'
+urls = ['https://calscape.org/loc-34.4136,-119.8497%20(93106)/cat-All-Plants/ord-popular/page-1/np-0?srchcr=sc6407fb7d19597', 
+        'https://calscape.org/loc-34.4136,-119.8497%20(93106)/cat-All-Plants/ord-popular/page-2/np-0?srchcr=sc6407fb7d19597']
 driver = webdriver.Chrome()
 common_names_list = []
 species_names_list = []
 all_names = []
 
 # Scrapes common and scientific names from 93106 page
+for url in urls:
 
-driver.get(url)
+    driver.get(url)
 
-common_names = driver.find_elements(By.CLASS_NAME, "common_name")
-for name in common_names:
-    if len(name.text) != 0:
-        common_names_list.append(name.text)
-    
-species_names = driver.find_elements(By.CLASS_NAME, "species")
-for name in species_names:
-    if len(name.text) != 0:
-        species_names_list.append(name.text)
-    
+    common_names = driver.find_elements(By.CLASS_NAME, "common_name")
+    for name in common_names:
+        if len(name.text) != 0:
+            common_names_list.append(name.text)
+        
+    species_names = driver.find_elements(By.CLASS_NAME, "species")
+    for name in species_names:
+        if len(name.text) != 0:
+            species_names_list.append(name.text)
+        
 for index in range(0, len(common_names_list)):
     all_names.append(f"{common_names_list[index]} ({species_names_list[index]})")
 
 # scrapes other info from each plant (goes to seperate plant url)
-'Marah-fabacea-(Wild-Cucumber)'
+
 plant_urls_base = 'https://calscape.org/loc-34.4136,-119.8497%20(93106)/Marah-fabacea-(Wild-Cucumber)?srchcr=sc63f5693746598'
 common_name_url = []
 species_url = []
@@ -62,6 +64,7 @@ for index in range(0, len(plant_url)):
 # adds all info and names to a dataframe
 
 df = pd.DataFrame(all_plant_info)
+
 df.insert(0, "Plant Name", all_names)
 
 df.to_csv('plantinfo.csv', index=False, encoding='utf-8')
